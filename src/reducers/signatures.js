a5f1184d5f05c158adee72be3608987a383df8ab
kaiwpost <kaiwpost@gmail.com>
2023-08-18 08:17:00 -0700

    Removed error logging

--- src/reducers/signatures.js
+++ src/reducers/signatures.js
@@ -1,7 +1,7 @@
 import * as types from "../actions/types";
 
 const Signatures = (state = {loaded: false, showCounts: false}, action) => {
-  console.log("SIG RED", action);
+
   switch (action.type) { 
     case types.CHANGE_ZOOM:
       return  Object.assign({}, state, {
